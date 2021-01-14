7a393615d11ef9cbe44ed2558a41a23de8b48a40
Anders Rune Jensen <arj03@protonmail.ch>
2021-01-14 11:07:51 +0100

    Tidy

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-
 module.exports = function (filter) {
   var value = null, listeners = [], oncers = []
+
   function trigger (_value) {
     value = _value
     var length = listeners.length
-    for(var i = 0; i< length && value === _value; i++) {
-      if (typeof listeners[i] === 'function') {
+    for (var i = 0; i < length && value === _value; i++) {
+      if (typeof listeners[i] === 'function')
         listeners[i](value)
-      }
       //if we remove a listener, must decrement i also
-
     }
     // decrement from length, incase a !immediately
     // listener is added during a trigger
     var l = oncers.length
     var _oncers = oncers
     oncers = []
-    while(l-- && _value === value) {
+    while (l-- && _value === value) {
       _oncers.shift()(value)
     }
   }
 
   function many (ready, immediately) {
     var i = listeners.push(ready) - 1
-    if(value !== null && immediately !== false) ready(value)
+    if (value !== null && immediately !== false) ready(value)
     return function () { //manually remove...
       //fast path, will happen if an earlier listener has not been removed.
-      if(listeners[i] !== ready)
+      if (listeners[i] !== ready)
         i = listeners.indexOf(ready)
       listeners.splice(i, 1)
     }
   }
 
   many.set = function (_value) {
-    if(filter ? filter(value, _value) : true) trigger(many.value = _value)
+    if (filter ? filter(value, _value) : true) trigger(many.value = _value)
     return many
   }
 
@@ -53,5 +51,3 @@ module.exports = function (filter) {
 
   return many
 }
-
-
